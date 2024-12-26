e935a2abe0417f2633d1001789624d91767d3094
JaromekArch <jakub.jaromin2004@gmail.com>
2024-12-27 00:31:10 +0100

    do poprawy subsections_number

--- Section.py
+++ Section.py
@@ -18,22 +18,41 @@ class Section:
 
 
     def point_section(n_angles, point):
-        for section_num in range(1,n_angles+1):
-            if point.getPhi() <= section_num * gu.GeometryUtils.section_angle_size(n_angles):
+        section_angle_size = gu.GeometryUtils.section_angle_size(n_angles)
+        for section_num in range(n_angles):
+            if point.getPhi() <= (section_num + 1)* section_angle_size:
                 return section_num
     
-    def points_in_section(n_angles, tab):
-        section = []
-        for i in range(n_angles):
-            section.append(list(filter(lambda x: x.getPhi() < (i+1)*gu.GeometryUtils.section_angle_size(n_angles) and x.getPhi() > i*gu.GeometryUtils.section_angle_size(n_angles), tab)))
+    def points_in_section(n_angles, points, section_num):
+        section_angle_size = gu.GeometryUtils.section_angle_size(n_angles)
+        section = [point for point in points if section_num * section_angle_size < point.getPhi() < (section_num + 1) * section_angle_size]
         return section
+    
+    def number_of_elements_in_section(n_angles, points, section_num):
+        return len(Section.points_in_section(n_angles, points, section_num))
+
+
+class SubSection:
+    def __init__(self, name, start, end):
+        self.name = name
+        self.start = start
+        self.end = end
+    
+    def __str__(self):
+        return f'{self.name} ({self.start}-{self.end})'
+    
+    def __repr__(self):
+        return str(self)
+    
+    def subsections_number(n_angles, points, section_num):
+        return abs(int(math.sqrt((Section.number_of_elements_in_section(n_angles, points, section_num)/len(points)) * 100))-2)
 
 if __name__ == '__main__':
     
-    n = 5
+    n = 100
     tab = [co.Coordinate(random.uniform(-10, 10), random.uniform(-10, 10)) for i in range(n)]
     for i in range(n):
-        ic(tab[i].getXY(),tab[i].getR(),tab[i].getPhi(), Section.point_section(5, tab[i]))
+        #ic(tab[i].getXY(),tab[i].getR(),tab[i].getPhi(), Section.point_section(5, tab[i]))
 
-
-    
+        #ic(SubSection.subsections_number(5, tab[i], Section.point_section(5, tab[i])))
+        print(" ")
