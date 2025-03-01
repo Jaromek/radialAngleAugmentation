3c9ecf4dcde0ea65fa96aa5872b3119d30a503c8
JaromekPC <jakub.jaromin2004@gmail.com>
2025-03-02 00:51:54 +0100

    cos chyba jest nie tak z losowaniem

--- radiousAngleMethod.py
+++ radiousAngleMethod.py
@@ -1,10 +1,34 @@
 import Augmentation as au
 import DataUtils as du
+import numpy as np
 
-def radiousAngleMethod(data, section_count, global_points_gen):
-    data = du.DataUtils.listed_class(data)
-    phiShiftByMaxRadious = du.DataUtils.getPhiShiftByMaxRadious(data, section_count)
-    du.DataUtils.addShiftPhi(data, -phiShiftByMaxRadious)
-    au.Augmentation(points=data, section_count=section_count, global_points_gen=global_points_gen)
-    du.DataUtils.addShiftPhi(data, phiShiftByMaxRadious)
-    return [point.getXY() for point in data]
+def radiousAngleMethodForSingleClass(dataset_data, dataset_target, section_count, global_points_gen):
+    
+    dataset_data = du.DataUtils.listed_class(dataset_data)
+    phiShiftByMaxRadious = du.DataUtils.getPhiShiftByMaxRadious(dataset_data, section_count)
+    du.DataUtils.addShiftPhi(dataset_data, -phiShiftByMaxRadious)
+    au.Augmentation(points=dataset_data, section_count=section_count, global_points_gen=global_points_gen)
+    du.DataUtils.addShiftPhi(dataset_data, phiShiftByMaxRadious)
+
+    return [point.getXY() for point in dataset_data], [dataset_target for targets in range(len(dataset_data))]
+
+def radiousAngleMethod(dataset_data, dataset_targets, section_count):
+
+    unique_classes, counts = np.unique(dataset_targets, return_counts=True)
+
+    for abstractClass in unique_classes:
+
+        difference = abs(np.max(unique_classes) - abstractClass)
+
+        if difference != 0:
+
+            augmented_data, augmented_target = radiousAngleMethodForSingleClass(dataset_data[dataset_targets == abstractClass], abstractClass, section_count, global_points_gen = difference)
+
+
+        else:
+            continue
+
+        dataset_data = np.concatenate((dataset_data, augmented_data), axis=0)
+        dataset_targets = np.concatenate((dataset_targets, augmented_target), axis=0)
+
+    return dataset_data, dataset_targets
